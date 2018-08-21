295780d69c5aa99602d21e9fdfdd28cca6708482
Unknown <jrafael_campossilva@hotmail.com>
2018-08-21 13:09:56 -0300

    rename AMQP

--- bank-query/src/main/java/br/com/coderef/configuration/AmqpConfiguration.java
+++ bank-query/src/main/java/br/com/coderef/configuration/AmqpConfiguration.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Slf4j
 @Configuration
-public class AxonConfiguration {
+public class AmqpConfiguration {
 
     @Bean
     public SpringAMQPMessageSource complaintEventsMethod(Serializer serializer) {
